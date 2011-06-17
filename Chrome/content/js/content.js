b4806ea874d16172e52dca27688a4c93945fa73b
brandon fisch <brandon.fisch@gmail.com>
2011-06-16 19:06:04 -0700

    setting up environment

---
+++ Chrome/content/js/content.js
@@ -0,0 +1,22 @@
+var $ff = false;
+
+//port object crossbrowser compatable
+
+var xBport = function(portName,portDesc=portName){
+	portName = chrome.extension.connect({name: portdesc}); 
+	this.addListener(callback) = portname.onMessage.addListener(callback);
+	this.postMessage(msgObj) = portname.postMessage(msgObj);
+};
+var 
+$ff?
+
+//everything is wrapped in JQuery 
+$(document).ready(function(){
+ //sets ff var 
+ if(!$.browser.webkit){
+	$ff = true;
+ }
+
+
+
+});
