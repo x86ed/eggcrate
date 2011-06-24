a194146fa592ff85b15829da532df4d2daae3da8
tobioatom <tobioatom@localhost>
2011-06-24 00:20:34 +0000

    testing clock

--- content/js/poc.js
+++ content/js/poc.js
@@ -71,19 +71,21 @@ var poc = function () {
 //window.addEventListener(poc.DOMEvent("load"), poc.run, false);
 
  $(document).ready(function(){
-  $('body').prepend('<canvas style="position:absolute;top:0;left:0" id="canvas1" width="200" height="200"></canvas>')
+ 
+$('body').prepend('<canvas style="position:absolute;top:0;left:0" id="canvas1" width="200" height="200"></canvas>')
+
 function sketchProc(processing) {  
-  // Override draw function, by default it will be called 60 times per second  
-  processing.draw = function() {  
-    // determine center and max clock arm length  
+// Override draw function, by default it will be called 60 times per second  
+    processing.draw = function() {  
+// determine center and max clock arm length  
     var centerX = processing.width / 2, centerY = processing.height / 2;  
     var maxArmLength = Math.min(centerX, centerY);  
-  
+    
     function drawArm(position, lengthScale, weight) {        
-      processing.strokeWeight(weight);  
-      processing.line(centerX, centerY,   
-        centerX + Math.sin(position * 2 * Math.PI) * lengthScale * maxArmLength,  
-        centerY - Math.cos(position * 2 * Math.PI) * lengthScale * maxArmLength);  
+        processing.strokeWeight(weight);
+        processing.line(centerX, centerY,
+            centerX + Math.sin(position * 2 * Math.PI) * lengthScale * maxArmLength,
+            centerY - Math.cos(position * 2 * Math.PI) * lengthScale * maxArmLength);  
     }  
   
     // erase background  
@@ -109,5 +111,5 @@ function sketchProc(processing) {
 var canvas = document.getElementById("canvas1");  
 // attaching the sketchProc function to the canvas  
 var p = new Processing(canvas, sketchProc);  
-// p.exit(); to detach it 
+  //p.exit(); to detach it 
 });
