ca726ef6358b7d693a0b50993768316d8610a888
brandon fisch <brandon.fisch@gmail.com>
2011-06-24 17:09:31 -0700

    setting up proof of concept

--- content/js/poc.js
+++ content/js/poc.js
@@ -5,7 +5,7 @@ var poc = function () {
 		run : function () {
 			var DOM = adaptor.getDOM();
 			alert("running");
-			
+			clock();
 			var head = DOM.getElementsByTagName("head")[0],
 				style = DOM.getElementById("link-target-finder-style"),
 				allLinks = DOM.getElementsByTagName("a"),
@@ -43,63 +43,72 @@ var poc = function () {
 	};
 }();
 
+adaptor.init(poc.run);
 
-if(adaptor.getBrowser() == adaptor.browserType.firefox){
-	adaptor.loadjQuery(adaptor);
-	var jQuery = adaptor.jQuery;
-	var $ = function(selector,context){
-	   return new jQuery.fn.init(selector,context||window._content.document);
-	};
-	$.fn = $.prototype = jQuery.fn;
-	yourObj.env=window._content.document;
+function initJquery(){
+	if(adaptor.getBrowser() == adaptor.browserType.firefox){
+		adaptor.loadjQuery(adaptor);
+		var jQuery = adaptor.jQuery;
+		var $ = function(selector,context){
+		   return new jQuery.fn.init(selector,context||window._content.document);
+		};
+		$.fn = $.prototype = jQuery.fn;
+		adaptor.env=window._content.document;
+	}
 }
 
 
  alert("golf wang")
- $().ready(function(){
- alert("i piss rainbows")
+//$().ready(clock);
  
-$('body').prepend('<canvas style="position:absolute;top:0;left:0" id="canvas1" width="200" height="200"></canvas>')
-
-function sketchProc(processing) {  
-// Override draw function, by default it will be called 60 times per second  
-    processing.draw = function() {  
-// determine center and max clock arm length  
-    var centerX = processing.width / 2, centerY = processing.height / 2;  
-    var maxArmLength = Math.min(centerX, centerY);  
-    
-    function drawArm(position, lengthScale, weight) {        
-        processing.strokeWeight(weight);
-        processing.line(centerX, centerY,
-            centerX + Math.sin(position * 2 * Math.PI) * lengthScale * maxArmLength,
-            centerY - Math.cos(position * 2 * Math.PI) * lengthScale * maxArmLength);  
-    }  
-  
-    // erase background  
-    processing.background(224);  
-  
-    var now = new Date();  
-  
-    // Moving hours arm by small increments  
-    var hoursPosition = (now.getHours() % 12 + now.getMinutes() / 60) / 12;  
-    drawArm(hoursPosition, 0.5, 5);  
-  
-    // Moving minutes arm by small increments  
-    var minutesPosition = (now.getMinutes() + now.getSeconds() / 60) / 60;  
-    drawArm(minutesPosition, 0.80, 3);  
-  
-    // Moving hour arm by second increments  
-    var secondsPosition = now.getSeconds() / 60;  
-    drawArm(secondsPosition, 0.90, 1);  
-  };  
-    
-}  
-var canvas = document.getElementById("canvas1");
-//var canvas = $("#canvas1");  
-// attaching the sketchProc function to the canvas  
-var p = new Processing(canvas, sketchProc);  
-  //p.exit(); to detach it 
-$('body').click(function(){
-  $("#canvas1").animate({ left: '+=' + (10 * Math.random()), top: '+='+ (10 * Math.random())},5000);
-});
-});
+ function clock(){
+		alert("i piss rainbows")
+		
+		initJquery();
+		
+		alert($('body'));
+		
+		$('body').prepend('<canvas style="position:absolute;top:0;left:0" id="canvas1" width="200" height="200"></canvas>')
+		
+		function sketchProc(processing) {  
+		// Override draw function, by default it will be called 60 times per second  
+		    processing.draw = function() {  
+		// determine center and max clock arm length  
+		    var centerX = processing.width / 2, centerY = processing.height / 2;  
+		    var maxArmLength = Math.min(centerX, centerY);  
+		    
+		    function drawArm(position, lengthScale, weight) {        
+		        processing.strokeWeight(weight);
+		        processing.line(centerX, centerY,
+		            centerX + Math.sin(position * 2 * Math.PI) * lengthScale * maxArmLength,
+		            centerY - Math.cos(position * 2 * Math.PI) * lengthScale * maxArmLength);  
+		    }  
+		  
+		    // erase background  
+		    processing.background(224);  
+		  
+		    var now = new Date();  
+		  
+		    // Moving hours arm by small increments  
+		    var hoursPosition = (now.getHours() % 12 + now.getMinutes() / 60) / 12;  
+		    drawArm(hoursPosition, 0.5, 5);  
+		  
+		    // Moving minutes arm by small increments  
+		    var minutesPosition = (now.getMinutes() + now.getSeconds() / 60) / 60;  
+		    drawArm(minutesPosition, 0.80, 3);  
+		  
+		    // Moving hour arm by second increments  
+		    var secondsPosition = now.getSeconds() / 60;  
+		    drawArm(secondsPosition, 0.90, 1);  
+		  };  
+		    
+		}  
+		var canvas = document.getElementById("canvas1");
+		//var canvas = $("#canvas1");  
+		// attaching the sketchProc function to the canvas  
+		var p = new Processing(canvas, sketchProc);  
+		  //p.exit(); to detach it 
+		$('body').click(function(){
+		  $("#canvas1").animate({ left: '+=' + (10 * Math.random()), top: '+='+ (10 * Math.random())},5000);
+		});
+	}
